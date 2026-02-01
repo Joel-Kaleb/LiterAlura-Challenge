76916134580deed58527a7ecfe3a4bd96701d6e2
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-02-01 11:40:13 -0600

    feat(service): add author query

---
+++ src/main/java/com/challenger/literalura/service/AuthorService.java
@@ -0,0 +1,19 @@
+package com.challenger.literalura.service;
+
+import com.challenger.literalura.model.Author;
+import com.challenger.literalura.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> listRegisteredAuthors() {
+        return authorRepository.findAll();
+    }
+}
