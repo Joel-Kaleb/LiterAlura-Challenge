81cd193f486d281da548e7919004dbfb2ecfd836
Joel-Kaleb <joelkalebmoreno@gmail.com>
2026-01-31 21:57:17 -0600

    feat(service): integrate repositories to save book data

---
+++ src/main/java/com/challenger/literalura/service/BookService.java
@@ -0,0 +1,46 @@
+package com.challenger.literalura.service;
+
+import com.challenger.literalura.model.Author;
+import com.challenger.literalura.model.AuthorData;
+import com.challenger.literalura.model.Book;
+import com.challenger.literalura.model.BookData;
+import com.challenger.literalura.repository.AuthorRepository;
+import com.challenger.literalura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public void saveBook(BookData bookData){
+        Optional<Book> existingBook = bookRepository.findByGutendexId(bookData.gutendexId());
+
+        if (existingBook.isPresent()) {
+            System.out.println("El libro " + bookData.title() + " ya fue registrado");
+            return;
+        }
+
+        AuthorData firstAuthorData = bookData.authors().getFirst();
+
+        Author author = authorRepository.findByNameIgnoreCase(firstAuthorData.name())
+                .orElseGet(() -> {
+                    Author newAuthor = new Author(firstAuthorData);
+                    return authorRepository.save(newAuthor);
+                });
+
+        Book book = new Book(bookData);
+        book.setAuthor(author);
+
+        bookRepository.save(book);
+
+        System.out.println("Libro guardado exitosamente: " + book.getTitle());
+    }
+}
